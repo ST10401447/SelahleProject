c7ce656f9001238e32b5de57c5e9dbc0c213997b
nothanaza <nothanazao@gmail.com>
2025-08-21 14:03:53 +0200

    mobile header

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+// Toggle mobile menu
+const menuToggle = document.getElementById("menu-toggle");
+const navMenu = document.getElementById("nav-menu");
+
+menuToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+});
+
 //TESTIMONIALS CAROUSEL//
 
 document.addEventListener("DOMContentLoaded", function () {
